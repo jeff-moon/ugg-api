fb80a0b6b6d0ccf29e2663cffb755a08d7898f3d
Jeffrey Moon <moon.jeffreyw@gmail.com>
2022-07-24 13:17:46 -0400

    Pushing

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import UDotGGApi from "./src/api.js";
+import fs from 'fs';
 
 const ugg = new UDotGGApi();
 (async () => {
     await ugg.initialize();
     const result = await ugg.getSummonerInfo('na1', 'xiaolinfunk');
 
-    console.log(JSON.stringify(result, null, 2));
+    fs.writeFileSync('it.json', JSON.stringify(result, null, 2));
 
     ugg.finalize();
 })();
